458d84a743585b3b9612c5d57858fce740ec5d37
vinjas <danielvinyastorres@gmail.com>
2021-08-04 11:00:27 +0200

    Combine todos and filters reducers

---
+++ src/store/reducer.js
@@ -0,0 +1,21 @@
+import { combineReducers } from "redux";
+
+import todosReducer from "../features/todos/todosReducer";
+import filtersReducer from "../features/todos/filtersReducer";
+
+const rootReducer = combineReducers({
+    todos: todosReducer,
+    filters: filtersReducer
+})
+
+export default rootReducer
+
+/* MANUAL FUNCTION
+
+export default function rootReducer(state = {}, action) {
+    return {
+        todos: todosReducer(state.todos, action),
+        filters: filtersReducer(state.filters, action)
+    }
+}
+*/
